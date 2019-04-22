ebb939a75d955be007f5696bb367b48ae9479240
rrdunn11 <rrdunn@gmail.com>
2019-04-22 15:33:21 -0700

    Complete solo work for react components

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {found: false
+        };
+    }
+
+    onListItemHover() {
+        this.setState({
+            found: !this.state.found
+        });
+    }
+
+    render() {
+        var style = {
+            fontWeight: this.state.found ? 'bold' : 'normal'
+        };
+
+        return (
+            <li style={style} 
+            onMouseEnter={this.onListItemHover.bind(this)}
+            onMouseLeave={this.onListItemHover.bind(this)}
+            >{this.props.specificItem}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul>
+        {props.groceryItems.map(groceryItem =>
+            <GroceryListItem specificItem={groceryItem}/>
+        )}
+    </ul>
+);
+
+var list = {groceryItems:['strawberry', 'apple']}
+ReactDOM.render(<GroceryList groceryItems = {list.groceryItems}/> ,document.getElementById('app'));
